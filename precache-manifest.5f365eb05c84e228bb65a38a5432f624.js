586354af732ecc7a3016d8c3fc0c71c17f5b9284
Rddavis1389 <rddavis1389@yahoo.com>
2021-10-21 22:05:57 -0600

    Updates

---
+++ precache-manifest.5f365eb05c84e228bb65a38a5432f624.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4329c90e75b6487f40a8a234d36acc6b",
+    "url": "/module-21-challenge-fe/index.html"
+  },
+  {
+    "revision": "fcac59578c3923036340",
+    "url": "/module-21-challenge-fe/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "84ee370e4110ca977acc",
+    "url": "/module-21-challenge-fe/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "fcac59578c3923036340",
+    "url": "/module-21-challenge-fe/static/js/2.409a8a27.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/module-21-challenge-fe/static/js/2.409a8a27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84ee370e4110ca977acc",
+    "url": "/module-21-challenge-fe/static/js/main.0fef2f43.chunk.js"
+  },
+  {
+    "revision": "232b3e90abcc08f54513",
+    "url": "/module-21-challenge-fe/static/js/runtime-main.5657f965.js"
+  }
+]);
