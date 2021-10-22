a6adc16fc6722a8aacf77afd4ebfe0475dfbd68a
Rddavis1389 <rddavis1389@yahoo.com>
2021-10-21 21:49:48 -0600

    front-end

---
+++ build/precache-manifest.05d9bb1b281a10f644718d0743877964.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c51b35aa9ec4e3c8a727d69bba24a94c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcac59578c3923036340",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "3c4efb3f44dff6c4cd03",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "fcac59578c3923036340",
+    "url": "/static/js/2.409a8a27.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.409a8a27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c4efb3f44dff6c4cd03",
+    "url": "/static/js/main.3c787d57.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
